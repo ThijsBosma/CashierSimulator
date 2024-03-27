e6eb3a6d9492879d1ba5dc8440a160c8eecd5d36
ThijsBosma <bosmathijs005@gmail.com>
2024-03-27 09:23:50 +0100

    Changed prefabs

---
+++ Assets/Scripts/ConveyorBeltMove.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBeltMove : MonoBehaviour
+{
+    [SerializeField] private Transform _StartPoint;
+    [SerializeField] private Transform _EndPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = Vector3.Lerp(_StartPoint.position, _EndPoint.position, 2);
+    }
+}
